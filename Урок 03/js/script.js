b62df82857d5e278a99bd711c571e586a001dbdc
Ilia Griniuk <iliagriniuk@gmail.com>
2021-02-01 21:28:51 +0300

    Выполнил урок 3

--- Урок 03/js/script.js
+++ Урок 03/js/script.js
@@ -1,51 +1,93 @@
-// присвоим значение месячного дохода
-let money = 60000;
+// получение значение месячного дохода
+let money = +prompt('Ваш месячный доход?');
+console.log(typeof money);
 
-// присвоим значение для дополнительного дохода
-let income = 'Freelance';
-
-// массив с доп расходами
-let addExpenses = 'Интернет, Такси, Коммуналка';
+// получения значений доп расходов
+let addExpenses = prompt(`Перечислите 
+возможные расходы за рассчитываемый период через запятую`);
+console.log(typeof addExpenses);
 
 // булевое значение для deposit
-let deposit = true;
+let deposit = confirm('Есть ли у вас депозит в банке?');
+console.log(typeof deposit);
+
+// период
+let period = 5;
+console.log('Период равен ', period ,' месяцев');
 
 // сумма для накопления
 let mission = 100000;
+console.log('Цель заработать ', mission, ' рублей');
 
-// период
-let period = 3;
+// Получаю значения статей расходов и трат
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount2 = +prompt('Во сколько это обойдется?');
 
-// вывод в консоль типов данных
-let dataType = '\t типы данных';
-let string = dataType.toUpperCase() + '\n' + 'money: тип ' + typeof money + '\n' +
- 'income: тип ' + typeof income + '\n' +
- 'deposit: тип ' +  typeof deposit;
+console.log(expenses1, expenses2);
+let myArr = [expenses1, expenses2];
+console.log(myArr);
 
-console.log(string);
+// получаю бюджета на месяц
+let budgetMonth = money - (amount1 + amount2);
+console.log('Бюджет на месяц: ', budgetMonth);
 
- // переменная разделителя в консоли
- let splitter = '--------------------------------' + '\n';
+// за сколько месяцев будет достигнута цель
+let goal = mission / budgetMonth;
+console.log('Цель будет достигнута за ', Math.ceil(goal), 'месяцев(-а)');
 
-// вывод длины строки addExpenses
-console.log(splitter + 'длина строки addExpenses: ' + addExpenses.length);
-
-// Вывести в консоль “Период равен (period) месяцев”
-console.log(splitter + 'Период равен ' + period + ' месяцам');
-
-// Вывести в консоль “Цель заработать (mission) рублей/долларов/гривен/юани”
-console.log(splitter + 'Цель заработать ' + mission + ' рублей');
-
-/* Привести строку addExpenses к нижнему регистру 
-и разбить строку на массив, вывести массив в консоль
-*/
-let addExpensesLower = addExpenses.toLowerCase();
-console.log(splitter, 
-    'Массив addExpenses приведен к нижнему регистру и разбит на массив: \n',
-     addExpensesLower.split(', '));
-
-// Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
-let budgetDay = money / 30;
+// Получаю значение budgetDay и дневной бюджет
+let budgetDay = budgetMonth / 30;
 
 // Вывести в консоль budgetDay
-console.log(splitter, 'Дневной бюджет = ', budgetDay);
+console.log('Бюджет на день = ', Math.floor(budgetDay));
+
+if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay >= 0) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay < 0) {
+    console.log('Что то пошло не так');
+}
+
+
+
+
+
+/*
+    код со второго урока
+*/
+
+// // присвоим значение для дополнительного дохода
+// let income = 'Freelance';
+
+// // вывод в консоль типов данных
+// let dataType = '\t типы данных';
+// let string = dataType.toUpperCase() + '\n' + 'money: тип ' + typeof money + '\n' +
+//  'income: тип ' + typeof income + '\n' +
+//  'deposit: тип ' +  typeof deposit;
+
+// console.log(string);
+
+//  // переменная разделителя в консоли
+//  let splitter = '--------------------------------' + '\n';
+
+// // вывод длины строки addExpenses
+// console.log(splitter + 'длина строки addExpenses: ' + addExpenses.length);
+
+// // Вывести в консоль “Период равен (period) месяцев”
+// console.log(splitter + 'Период равен ' + period + ' месяцам');
+
+// // Вывести в консоль “Цель заработать (mission) рублей/долларов/гривен/юани”
+// console.log(splitter + 'Цель заработать ' + mission + ' рублей');
+
+// /* Привести строку addExpenses к нижнему регистру 
+// и разбить строку на массив, вывести массив в консоль
+// */
+// let addExpensesLower = addExpenses.toLowerCase();
+// console.log(splitter, 
+//     'Массив addExpenses приведен к нижнему регистру и разбит на массив: \n',
+//      addExpensesLower.split(', '));
