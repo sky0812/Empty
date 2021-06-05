883a690283684b5af83668db36474863ea7d340c
Ilia Griniuk <iliagriniuk@gmail.com>
2021-06-05 18:15:52 +0300

    сделал 18й

---
+++ script/script.js
@@ -0,0 +1,57 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // eslint-disable-next-line strict
+    'use strict';
+
+    // timer
+    function countTimer(deadline) {
+        const timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaining() {
+            const dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+
+            return { timeRemaining, hours, minutes, seconds };
+        }
+
+        function updateClock() {
+            const timer = getTimeRemaining();
+
+            if (timer.hours < 10) {
+                timerHours.textContent = '0' + timer.hours;
+            } else {
+                timerHours.textContent = timer.hours;
+            }
+
+            if (timer.minutes < 10) {
+                timerMinutes.textContent = '0' + timer.minutes;
+            } else {
+                timerMinutes.textContent = timer.minutes;
+            }
+
+            if (timer.seconds < 10) {
+                timerSeconds.textContent = '0' + timer.seconds;
+            } else {
+                timerSeconds.textContent = timer.seconds;
+            }
+
+            if (timer.timeRemaining > 0) {
+                setTimeout(updateClock, 1000);
+            } else {
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+            }
+        }
+
+        updateClock();
+    }
+
+    countTimer('10 jun 2021');
+
+});
